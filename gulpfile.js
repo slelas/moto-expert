b0b7390c277aac13f06c37b35d36a95711aa0607
slelas <stipe@dump.hr>
2018-06-22 19:10:42 +0200

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', ['compileSass', 'watch']);
+
+gulp.task('compileSass', function () {
+    return gulp.src('./styles/main.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(cleanCSS())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('watch', ['browserSync', 'compileSass'], function () {
+    gulp.watch('styles/**/*.scss', ['compileSass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+})
