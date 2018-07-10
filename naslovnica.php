a2fe348fc62342dbf485d7b91c2367649efb45ad
slelas <stipe@dump.hr>
2018-07-10 11:17:05 +0200

    Most of mobile homepage done

---
+++ naslovnica.php
@@ -0,0 +1,165 @@
+<?php require_once( 'couch/cms.php' ); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/dist/main.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,900" rel="stylesheet">
+        <title>Moto Expert</title>
+    </head>
+    <body>
+        <cms:embed 'nav.php' />
+        <section class="samozanav"></section>
+        <section class="redirect">
+            <div class="redirect__main-info-container">
+                <img class="redirect__main-info-image" src="/images/motor/svi-motori.png">
+                <div class="redirect__main-info">
+                    <h2>Motore koje držimo khaled ipsum is a major key to success in Croatia!</h2>
+                    <p>Lorem Khaled Ipsum is a major key to success. Look at the sunset, life is amazing, life is beautiful, life is what you make it. It’s important to use cocoa butter.</p>
+                </div>
+            </div>
+            <div class="redirect__side-info-container">
+                <a class="redirect__side-info" href="#">
+                    <img class="redirect__side-info-icon" src="/images/motor/rabljeni-motori.png">
+                    <div class="redirect__side-info-label">Rabljena vozila</div>
+                    <img class="redirect__side-info-arrow" src="/images/arrow-v2.png">
+                </a>
+                <a class="redirect__side-info" href="#">
+                    <img class="redirect__side-info-icon" src="/images/motor/novi-motori.png">
+                    <div class="redirect__side-info-label">Nova vozila</div>
+                    <img class="redirect__side-info-arrow" src="/images/arrow-v2.png">
+                </a>
+                <a class="redirect__side-info" href="#">
+                    <img class="redirect__side-info-icon" src="/images/motor/u-dolasku.png">
+                    <div class="redirect__side-info-label">U dolasku</div>
+                    <img class="redirect__side-info-arrow" src="/images/arrow-v2.png">
+                </a>
+            </div>
+        </section>
+        <section class="process-section">
+            <h2>Lorem Khaled Ipsum is a major key to success in Croatia!</h2>
+            <p>Lorem Khaled Ipsum is a major key to success. Look at the sunset, life is amazing, life is beautiful, life is what you make it. It’s important to use cocoa butter.</p>
+            <div class="processes">
+                <div class="process">
+                    <img class="process__icon" src="/images/motor/oval-1.png">
+                    <span>Vidi koji ti se motor sviđa</span>
+                </div>
+                <div class="process">
+                    <img class="process__icon" src="/images/motor/oval-2.png">
+                    <span>Dođi i pogledaj motor</span>
+                </div>
+                <div class="process">
+                    <img class="process__icon" src="/images/motor/oval-3.png">
+                    <span>Baci đir s njim</span>
+                </div>
+                <div class="process">
+                    <img class="process__icon" src="/images/motor/oval-4.png">
+                    <span>Potpiši dokumente i uživaj u vožnji</span>
+                </div>
+            </div>
+        </section>
+        <section class="why">
+            <div class="why__container">
+                <h2 class="why__title">Lorem Khaled Ipsum is a major key to success.</h2>
+                <div class="why__card-container">
+                    <div class="why__card clearfix">
+                        <img src="/images/motor/why-1.png">
+                        <div class="why__card-wrapper">
+                            <h3>Brzo</h3>
+                            <p>Lorem Khaled Ipsum is a major key to success. Look at the sunset.</p>
+                        </div>
+                    </div>
+                    <div class="why__card clearfix">
+                        <img src="/images/motor/why-2.png">
+                        <div class="why__card-wrapper">
+                            <h3>Sigurno</h3>
+                            <p>Lorem Khaled Ipsum is a major key to success. Look at the sunset.</p>
+                        </div>
+                    </div>
+                    <div class="why__card clearfix">
+                        <img src="/images/motor/why-3.png">
+                        <div class="why__card-wrapper">
+                            <h3>Profesionalno</h3>
+                            <p>Lorem Khaled Ipsum is a major key to success. Look at the sunset.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="customers">
+            <div class="customers__container">
+                <h3>Naša najbolja reklama su naši zadovoljni kupci</h3>
+                <p>Lorem Khaled Ipsum is a major key to success. The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! Cloth talk. To succeed you must believe. </p>
+            </div>
+            <div class="customers__slider">
+                <div class="customer__card">
+                    <div class="card__info-container">
+                        <div class="card__model">HONDA CBR</div>
+                        <div class="card__stars">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                        </div>
+                        <div class="card__quote">“Lorem Khaled Ipsum is a major key to success. Lorem Khaled Ipsum is a major key to success. They don’t want us to win. Special cloth alert.“</div>
+                        <div class="card__person">M. Viđak</div>
+                    </div>
+                </div>
+                <div class="customer__card">
+                    <div class="card__info-container">
+                        <div class="card__model">HONDA CBR</div>
+                        <div class="card__stars">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                        </div>
+                        <div class="card__quote">“Lorem Khaled Ipsum is a major key to success. Special cloth alert.“</div>
+                        <div class="card__person">M. Viđak</div>
+                    </div>
+                </div>
+                <div class="customer__card">
+                    <div class="card__info-container">
+                        <div class="card__model">HONDA CBR</div>
+                        <div class="card__stars">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                        </div>
+                        <div class="card__quote">“Lorem Khaled Ipsum is a major key to success. They don’t want us to win. Special cloth alert.“</div>
+                        <div class="card__person">M. Viđak</div>
+                    </div>
+                </div>
+                <div class="customer__card">
+                    <div class="card__info-container">
+                        <div class="card__model">HONDA CBR</div>
+                        <div class="card__stars">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                            <img class="card__star" src="/images/star.png">
+                        </div>
+                        <div class="card__quote">“They don’t want us to win. Special cloth alert.“</div>
+                        <div class="card__person">M. Viđak</div>
+                    </div>
+                </div>
+            </div>
+            <a class="customer__redirect clearfix" href="">
+                <img class="customer__redirect-icon" src="/images/customer-redirect.png">
+                <div class="customer__redirect-text">
+                    <div class="customer__redirect-top">Želite i vi postati zadovoljni kupac?</div>
+                    <div class="customer__redirect-bottom">Provjerite motore u ponudi <img src="/images/arrow-v3.png" /></div>
+                </div>
+            </a>
+        </section>
+        <cms:embed 'footer.php' />
+    </body>
+</html>
+<?php COUCH::invoke(); ?>
